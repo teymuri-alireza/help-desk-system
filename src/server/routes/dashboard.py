c3c9bc556e6921f5b20b7fd9360cf806dad539ad
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-06-12 13:24:58 +0330

    feat(ui): serve dashboard HTML and CSS files
    Added GET method to dashboard route in dashboard.py to serve the static
    files basic structure.

--- src/server/routes/dashboard.py
+++ src/server/routes/dashboard.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from src.server.dependencies import get_static_path
+
+TEMPLATES_DIR = get_static_path()[0]
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 router = APIRouter(prefix="/dashboard", tags=["dashboard"])
 
 
 @router.get("")
-def dashboard():
-    return {"response": "Dashboard Page"}
+def dashboard(request: Request):
+    return templates.TemplateResponse(request=request, name="dashboard.html")
