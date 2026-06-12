36cd26c119f066663c54ecb90d7516ac42437342
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-06-12 16:22:59 +0330

    feat(api): complete /login POST using a temp session
    The /login POST endpoint uses a temporary session dictionary to store
    errors. The errors may occur from the HelpDeskCore methods.

--- src/server/routes/auth.py
+++ src/server/routes/auth.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form, status
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
-from src.server.dependencies import get_static_path
+from src.server.dependencies import get_static_path, get_helpdesk
 
 TEMPLATES_DIR = get_static_path()[0]
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+
+# Create temporary session to manage errors
+temp_session = {}
 
 # Serve the authentication page
 @router.get("")
@@ -19,8 +23,15 @@ def sign_up():
     return {"response": "Sign Up Page"}
 
 @router.post("/login")
-def log_in():
-    return {"response": "Log In Page"}
+def log_in(request: Request, username: str = Form(...)):
+    helpdesk = get_helpdesk()
+    user_exist = helpdesk.authentication_api(action="login", username=username)
+    if user_exist:
+        return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
+    else:
+        global temp_session
+        temp_session["error"] = "کاربر یافت نشد"
+        return RedirectResponse(url="/auth", status_code=status.HTTP_303_SEE_OTHER)
 
 # Session management
 @router.get("/me")
