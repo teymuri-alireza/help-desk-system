a4a7ccb60d84a4c66cbaa2c70040453ece49e82e
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-06-15 19:46:15 +0330

    feat(api): complete /users/user_id PATCH
    Completed PATCH method for updating users, and add form in
    show_user.html.

--- src/server/routes/users.py
+++ src/server/routes/users.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Request, status, Path
+from fastapi import APIRouter, Request, status, Path, Form
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from src.server.dependencies import get_helpdesk, get_current_user, get_static_path
-from src.database.tables import Role
+from src.database.tables import Role, User, UserStatus
 
 TEMPLATES_DIR, STATIC_DIR = get_static_path()
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
@@ -48,6 +48,8 @@ def show_user(request: Request, user_id: int = Path(...)):
             context = {
                 "request": request,
                 "user": user_to_show,
+                "Role": Role,
+                "UserStatus": UserStatus,
             }
             response = templates.TemplateResponse(
                 request=request,
@@ -68,5 +70,25 @@ def new_user():
     return {"response": "New User Page"}
 
 @router.patch("/{user_id}")
-def patch_user():
-    return {"response": "Patch User Page"}
+def patch_user(
+        request: Request, 
+        user_id: int = Path(...), 
+        name: str = Form(...), 
+        email: str = Form(...), 
+        username: str = Form(...), 
+        role: str = Form(...), 
+        user_status: str = Form(...)
+    ):
+    try:
+        user_username = get_current_user(request=request)
+    except:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_303_SEE_OTHER)
+    helpdesk = get_helpdesk()
+    found_user = helpdesk.admin_api(action="find_user_by_username", username=user_username)
+    # Check if user is admin first
+    if found_user is not None and found_user.role == Role.SYSTEM_ADMIN:
+        user_to_update = User(name=name, email=email, username=username, role=role, status=user_status)
+        helpdesk.admin_api("update_user", user=user_to_update, user_id=user_id)
+        return RedirectResponse(url=f"/users/{user_id}", status_code=status.HTTP_303_SEE_OTHER)
+    else:
+        return {"response": "You don't have access"}
