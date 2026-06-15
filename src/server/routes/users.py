acd5f66f50d77d2358ec9d06ca6f450556af0f88
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-06-15 17:13:43 +0330

    feat(api): complete /users/user_id GET endpoint
    Completed show_user() and add show_user.html

--- src/server/routes/users.py
+++ src/server/routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, status
+from fastapi import APIRouter, Request, status, Path
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from src.server.dependencies import get_helpdesk, get_current_user, get_static_path
@@ -34,8 +34,34 @@ def list_users(request: Request):
         return {"response": "not found"}
 
 @router.get("/{user_id}")
-def show_user():
-    return {"response": "Show User Page"}
+def show_user(request: Request, user_id: int = Path(...)):
+    try:
+        user_username = get_current_user(request=request)
+    except:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_303_SEE_OTHER)
+    helpdesk = get_helpdesk()
+    found_user = helpdesk.admin_api(action="find_user_by_username", username=user_username)
+    # Check if user is admin first
+    if found_user is not None and found_user.role == Role.SYSTEM_ADMIN:
+        user_to_show = helpdesk.admin_api(action="find_user", user_id=user_id)
+        if user_to_show is not None:
+            context = {
+                "request": request,
+                "user": user_to_show,
+            }
+            response = templates.TemplateResponse(
+                request=request,
+                name="show_user.html",
+                context=context
+            )
+            return response
+        else:
+            return templates.TemplateResponse(
+                request=request,
+                name="show_user.html",
+            )
+    else:
+        return {"response": "You don't have access"}
 
 @router.post("")
 def new_user():
