71979c88af43d3cdae2ac69550da7c73ea1bc9b3
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-06-15 16:52:43 +0330

    feat(api): complete /users GET
    Completed the list_users() and added users.html.

--- src/server/routes/users.py
+++ src/server/routes/users.py
@@ -1,11 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, status
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from src.server.dependencies import get_helpdesk, get_current_user, get_static_path
+from src.database.tables import Role
+
+TEMPLATES_DIR, STATIC_DIR = get_static_path()
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
 @router.get("")
-def list_users():
-    return {"response": "List Users Page"}
+def list_users(request: Request):
+    try:
+        user_username = get_current_user(request=request)
+    except:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_303_SEE_OTHER)
+    helpdesk = get_helpdesk()
+    found_user = helpdesk.admin_api(action="find_user_by_username", username=user_username)
+    if found_user is not None and found_user.role == Role.SYSTEM_ADMIN:
+        users_list = helpdesk.admin_api(action="list_users")
+        context = {
+            "request": request,
+            "users_list": users_list,
+        }
+        response = templates.TemplateResponse(
+            request=request,
+            name="users.html",
+            context=context
+        )
+        return response
+    else:
+        return {"response": "not found"}
 
 @router.get("/{user_id}")
 def show_user():
