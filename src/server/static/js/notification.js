826a20297adb8982491ae13ee396134f5dca3c69
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-06-22 10:33:11 +0330

    refactor: extract alert timeout logic into a standalone file

--- src/server/static/js/notification.js
+++ src/server/static/js/notification.js
@@ -93,28 +93,3 @@ document
   });
 
 loadNotifications();
-
-document.addEventListener("DOMContentLoaded", () => {
-    const ticketAlert = document.getElementById("ticket-flash-alert");
-    const userAlert = document.getElementById("user-flash-alert");
-
-    if (ticketAlert) {
-        setTimeout(() => {
-            ticketAlert.style.opacity = "0";
-
-            setTimeout(() => {
-                ticketAlert.remove();
-            }, 500);
-        }, 3500);
-    }
-
-    if (userAlert) {
-        setTimeout(() => {
-            userAlert.style.opacity = "0";
-
-            setTimeout(() => {
-                userAlert.remove();
-            }, 500);
-        }, 3500);
-    }
-});
