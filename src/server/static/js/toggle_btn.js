551b51a83c4bdb76e350b4ca4b1f8e0c139855e4
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-06-22 10:37:45 +0330

    refactor: extract button toggle logic into a standalone file

--- src/server/static/js/toggle_btn.js
+++ src/server/static/js/toggle_btn.js
@@ -1,4 +1,5 @@
 // Button to open and close form
+// Used in show_ticket.html, and show_user.html
 const toggleBtn = document.getElementById('toggleFormBtn');
 const editForm = document.getElementById('editForm');
 
@@ -9,5 +10,32 @@ toggleBtn.addEventListener('click', function() {
     } else {
         editForm.style.display = 'none';
         toggleBtn.textContent = 'نمایش فرم';
+    }
+});
+
+// Used in admin_dashboard.html
+const adminDashboardToggleBtn = document.getElementById('toggleFormBtn');
+const newTicketForm = document.getElementById('newTicketForm');
+
+adminDashboardToggleBtn.addEventListener('click', function() {
+    if (newTicketForm.style.display === 'none') {
+        newTicketForm.style.display = 'block';
+        adminDashboardToggleBtn.textContent = 'بستن فرم';
+    } else {
+        newTicketForm.style.display = 'none';
+        adminDashboardToggleBtn.textContent = 'نمایش فرم';
+    }
+});
+
+const toggleUserFormBtn = document.getElementById('toggleUserFormBtn');
+const newUserForm = document.getElementById('newUserForm');
+
+toggleUserFormBtn.addEventListener('click', function() {
+    if (newUserForm.style.display === 'none') {
+        newUserForm.style.display = 'block';
+        toggleUserFormBtn.textContent = 'بستن فرم';
+    } else {
+        newUserForm.style.display = 'none';
+        toggleUserFormBtn.textContent = 'نمایش فرم';
     }
 });
