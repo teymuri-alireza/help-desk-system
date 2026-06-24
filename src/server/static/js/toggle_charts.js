251310c6b7ae0ab19300b205a8c1fecdf7b10cc7
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-06-24 21:47:01 +0330

    feat(ui): divide users and tickets charts using JS code

---
+++ src/server/static/js/toggle_charts.js
@@ -0,0 +1,19 @@
+const users_charts_btn = document.getElementById("users-charts-btn");
+const tickets_charts_btn = document.getElementById("tickets-charts-btn");
+const users_charts = document.getElementById("users-charts");
+const tickets_charts = document.getElementById("tickets-charts");
+
+tickets_charts.style.display = "none";
+
+users_charts_btn.addEventListener("click", async (e) => {
+    users_charts_btn.classList.add("stats-active");
+    tickets_charts_btn.classList.remove("stats-active");
+    users_charts.style.display = "block";
+    tickets_charts.style.display = "none";
+});
+tickets_charts_btn.addEventListener("click", async (e) => {
+    tickets_charts_btn.classList.add("stats-active");
+    users_charts_btn.classList.remove("stats-active");
+    tickets_charts.style.display = "block";
+    users_charts.style.display = "none";
+});
