4382322bca6a9750441e4d2a9dba3ce112cbbd43
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-06-28 15:15:49 +0330

    feat(api): give statistics page access to IT managers

--- src/server/routes/dashboard.py
+++ src/server/routes/dashboard.py
@@ -120,7 +120,7 @@ def stats(request: Request):
     try:
         helpdesk = get_helpdesk()
         current_user = helpdesk.admin_api.find_user_by_username(username=user_username)
-        if current_user.role not in (Role.SYSTEM_ADMIN, Role.HELP_DESK_MANAGER):
+        if current_user.role not in (Role.SYSTEM_ADMIN, Role.IT_MANAGER, Role.HELP_DESK_MANAGER):
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access denied")
         all_tickets_count, active_tickets_count, not_assigned_tickets, last_created = helpdesk.statistics_api.ticket_stats()
         all_users_count, active_users_count = helpdesk.statistics_api.users_stats()
