ca74c9ca25343be9c0992234ac30338777be5098
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-07-06 16:22:00 +0330

    refactor: pass exception for not found elements in toggle_btn.js

--- src/server/static/js/toggle_btn.js
+++ src/server/static/js/toggle_btn.js
@@ -1,41 +1,53 @@
 // Button to open and close form
 // Used in show_ticket.html, and show_user.html
-const toggleBtn = document.getElementById('toggleFormBtn');
-const editForm = document.getElementById('editForm');
+try {
+    const toggleBtn = document.getElementById('toggleFormBtn');
+    const editForm = document.getElementById('editForm');
 
-toggleBtn.addEventListener('click', function() {
-    if (editForm.style.display === 'none') {
-        editForm.style.display = 'block';
-        toggleBtn.textContent = 'بستن فرم';
-    } else {
-        editForm.style.display = 'none';
-        toggleBtn.textContent = 'نمایش فرم';
-    }
-});
+    toggleBtn.addEventListener('click', function() {
+        if (editForm.style.display === 'none') {
+            editForm.style.display = 'block';
+            toggleBtn.textContent = 'بستن فرم';
+        } else {
+            editForm.style.display = 'none';
+            toggleBtn.textContent = 'نمایش فرم';
+        }
+    });
+} catch {
+    // Pass execption if elements with id don't exist
+}
 
 // Used in admin_dashboard.html
-const adminDashboardToggleBtn = document.getElementById('toggleFormBtn');
-const newTicketForm = document.getElementById('newTicketForm');
+try {
+    const adminDashboardToggleBtn = document.getElementById('toggleFormBtn');
+    const newTicketForm = document.getElementById('newTicketForm');
 
-adminDashboardToggleBtn.addEventListener('click', function() {
-    if (newTicketForm.style.display === 'none') {
-        newTicketForm.style.display = 'block';
-        adminDashboardToggleBtn.textContent = 'بستن فرم';
-    } else {
-        newTicketForm.style.display = 'none';
-        adminDashboardToggleBtn.textContent = 'نمایش فرم';
-    }
-});
+    adminDashboardToggleBtn.addEventListener('click', function() {
+        if (newTicketForm.style.display === 'none') {
+            newTicketForm.style.display = 'block';
+            adminDashboardToggleBtn.textContent = 'بستن فرم';
+        } else {
+            newTicketForm.style.display = 'none';
+            adminDashboardToggleBtn.textContent = 'نمایش فرم';
+        }
+    });
+} catch {
+    // Pass execption if elements with id don't exist
+}
 
-const toggleUserFormBtn = document.getElementById('toggleUserFormBtn');
-const newUserForm = document.getElementById('newUserForm');
+try {
+    const toggleUserFormBtn = document.getElementById('toggleUserFormBtn');
+    const newUserForm = document.getElementById('newUserForm');
 
-toggleUserFormBtn.addEventListener('click', function() {
-    if (newUserForm.style.display === 'none') {
-        newUserForm.style.display = 'block';
-        toggleUserFormBtn.textContent = 'بستن فرم';
-    } else {
-        newUserForm.style.display = 'none';
-        toggleUserFormBtn.textContent = 'نمایش فرم';
-    }
-});
+    toggleUserFormBtn.addEventListener('click', function() {
+        if (newUserForm.style.display === 'none') {
+            newUserForm.style.display = 'block';
+            toggleUserFormBtn.textContent = 'بستن فرم';
+        } else {
+            newUserForm.style.display = 'none';
+            toggleUserFormBtn.textContent = 'نمایش فرم';
+        }
+    });
+} catch {
+    // Pass execption if elements with id don't exist
+}
