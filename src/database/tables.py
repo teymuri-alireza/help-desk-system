fd1d92e6a6af2710b1ae7ffb4e54c6040ec4be00
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-07-15 21:55:41 +0330

    feat(db): replace IT field with Technical Services
    Replaced Information technology field with Technical Services in
    the DepartmentSchema class. The Information technology field was
    a broad term and not specific enough.

--- src/database/tables.py
+++ src/database/tables.py
@@ -97,8 +97,8 @@ class DepartmentSchema(Enum):
     USERS_SUPPORT = "Users Support"
     NETWORK = "Network"
     UNIVERSITY_SYSTEMS = "University Systems"
+    TECHNICAL_SERVICES = "Technical Services"
     INFORMATION_SECURITY = "Information Security"
-    INFORMATION_TECHNOLOGY = "Information technology"
 
     @property
     def fa(self):
@@ -106,8 +106,8 @@ class DepartmentSchema(Enum):
             self.USERS_SUPPORT: "پشتیبانی کاربران",
             self.NETWORK: "شبکه و اینترنت",
             self.UNIVERSITY_SYSTEMS: "سامانه‌های دانشگاهی",
+            self.TECHNICAL_SERVICES: "خدمات فنی",
             self.INFORMATION_SECURITY: "امنیت اطلاعات",
-            self.INFORMATION_TECHNOLOGY: "فناوری اطلاعات",
         }[self]
 
 
